878ef675129305ab57c0e644b6910adbdbdea9b2
Emelda-debug <emeldamada4@gmail.com>
2024-03-24 19:35:50 +0000

    advancedd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@
 
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => '~/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => '~/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 
