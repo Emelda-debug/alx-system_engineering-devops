6e28b47ff2034be0ac3bd56ee070ad9aa95bcdd1
Emelda-debug <emeldamada4@gmail.com>
2024-05-07 14:23:56 +0000

    corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,17 +4,17 @@ number of subscribers (not active users, total subscribers)
 for a given subreddit. If an invalid subreddit is given,
 the function should return 0
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the total number of subscribers"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """
+    Queries the Reddit API and returns the number of total subscribers
+    """
+    sub_url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'User-Agent'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    subscriber_num = response.json().get('data').get('subscribers')
+    return subscriber_num
